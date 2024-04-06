6f2562c1cddf3637d16bb0164e7de1f398737c43
Esteban Correa <estebancorrea405@gmail.com>
2024-04-05 22:25:13 -0500

    Colocando credenciales de la BD en el archivo env

--- src/databases/config.ts
+++ src/databases/config.ts
@@ -1,9 +1,12 @@
 import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const dbConnection = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'Ec1007256470',
-    database: 'event_managenement_db',
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     port: 5432
 });
